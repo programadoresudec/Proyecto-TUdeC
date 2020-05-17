63fbcbf7fff7335842ee71ab3333504ed08dffda
Miguel Manrique <miguelmanriquetellez@gmail.com>
2020-05-17 16:33:56 -0500

    Implementación expulsión de alumnos

---
+++ tudec/Vistas/Cursos/ExpulsarAlumnos.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Vistas/MasterPage.master" AutoEventWireup="true" CodeFile="ExpulsarAlumnos.aspx.cs" Inherits="Vistas_Cursos_ExpulsarAlumnos" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContentMaster" Runat="Server">
+
+    <link href="../../App_Themes/Estilos/Estilos.css" rel="stylesheet" />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <center>
+
+
+        <asp:GridView style="width: 60%;" CssClass="tablas" ID="tablaUsuarios" runat="server" AllowPaging="True" AutoGenerateColumns="False" DataSourceID="usuariosDataSource1" OnRowDataBound="tablaUsuarios_RowDataBound">
+            <Columns>
+                <asp:BoundField DataField="NombreDeUsuario" HeaderText="Usuarios" SortExpression="NombreDeUsuario" />
+            </Columns>
+
+
+
+
+        </asp:GridView>
+
+
+        <asp:ObjectDataSource ID="usuariosDataSource1" runat="server" SelectMethod="GetUsuarios" TypeName="DaoUsuario">
+            <SelectParameters>
+                <asp:SessionParameter Name="curso" SessionField="cursoSeleccionadoParaExpulsarAlumnos" Type="Object" />
+            </SelectParameters>
+        </asp:ObjectDataSource>
+
+
+    </center>
+
+    <br />
+
+
+</asp:Content>
+
---
+++ tudec/Vistas/Cursos/ExpulsarAlumnos.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Vistas_Cursos_ExpulsarAlumnos : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        tablaUsuarios.DataBind();
+
+    }
+
+    protected void tablaUsuarios_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+
+        GridViewRow fila = (GridViewRow)e.Row;
+
+        if (fila.RowIndex > -1)
+        {
+
+            TableCell celdaNombre = fila.Cells[0];
+
+            LinkButton hiperEnlaceCalificarExamen = new LinkButton();
+            hiperEnlaceCalificarExamen.Text = celdaNombre.Text;
+            hiperEnlaceCalificarExamen.Click += new EventHandler(Expulsar);
+
+            celdaNombre.Controls.Add(hiperEnlaceCalificarExamen);
+
+        }
+
+    }
+
+    public void Expulsar(object sender, EventArgs e)
+    {
+
+        LinkButton hiperEnlace = (LinkButton)sender;
+
+        DaoUsuario gestorUsuarios = new DaoUsuario();
+        GestionCurso gestorCursos = new GestionCurso();
+
+        EUsuario usuario = gestorUsuarios.GetUsuario(hiperEnlace.Text);
+        ECurso curso = (ECurso)Session[Constantes.CURSO_SELECCIONADO_PARA_EXPULSAR_ALUMNOS];
+
+        EInscripcionesCursos inscripcion = gestorCursos.GetInscripcion(usuario, curso);
+
+        Base.Eliminar(inscripcion);
+
+        Response.Redirect("~/Vistas/Cursos/ExpulsarAlumnos.aspx");
+
+    }
+
+}
